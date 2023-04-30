845230f43d32f8ffd6aa412328ba19680ec0a1ac
Slardarz <WARpoks29>
2023-04-30 21:20:17 +0200

    initial database set up - user table created

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -1,9 +1,11 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Domain.Models;
 
 public class User
 {
+    [Key]
     public int Id { get; set; }
 
     public string Email { get; set; }
